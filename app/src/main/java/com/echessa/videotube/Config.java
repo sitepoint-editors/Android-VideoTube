cef46f86e153af57e9072a44890df44ebfefd755
Chris Ward <chriswhward@gmail.com>
2015-08-25 13:35:32 +0200

    First and only commit

---
+++ app/src/main/java/com/echessa/videotube/Config.java
@@ -0,0 +1,13 @@
+package com.echessa.videotube;
+
+/**
+ * Created by echessa on 7/17/15.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "YOUR API KEY";
+
+}
